976178013d3bbaa17110c13033ce5bb68f812410
m10hit <mohitchauhan10@gmail.com>
2020-02-18 00:08:45 +1100

    Level 2 - Database Encryption

---
+++ app.js
@@ -0,0 +1,72 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const eejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = new mongoose.Schema({
+  email:String,
+  password:String
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function(req,res){
+  res.render("home");
+});
+
+app.get("/login", function(req,res){
+  res.render("login");
+});
+
+app.get("/register", function(req,res){
+  res.render("register");
+});
+
+app.post("/register", function(req,res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+newUser.save(function(err){
+  if(err){
+    console.log(err);
+  }else{
+    res.render("secrets");
+  }
+});
+});
+
+app.listen(3000, function(){
+  console.log("Server started running on port 3000");
+});
+
+app.post("/login", function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email:username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});

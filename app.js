c0700b4d9a1dd00bbb8dbc5d523c2748c2cdfd84
mcha2807 <mcha2807@uni.sydney.edu.au>
2020-07-12 02:47:28 +1000

    change in app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-mohit:test123@cluster0.hscgv.mongodb.net/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({

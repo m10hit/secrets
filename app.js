5ddf8021308392edfc23b9c57b16c7ea9f3ce73a
mcha2807 <mcha2807@uni.sydney.edu.au>
2020-07-12 02:53:41 +1000

    change gitignore, app.js and package.json files

--- app.js
+++ app.js
@@ -191,7 +191,11 @@ app.post("/login", function(req,res){
   });
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function(){
-  console.log("Server started running on port 3000");
+app.listen(port, function(){
+  console.log("Server has started running successfully");
 });
